e3cce09c27f01fc87655599dd748b1bf1606d238
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-02-18 15:12:21 +0000

    Improve histogram

--- src/plot_histogram.py
+++ src/plot_histogram.py
@@ -7,8 +7,9 @@ length_counts = map(lambda arr: int(arr[1]), lines)
 avg = sum([x * y for x, y in zip(tweet_lengths, length_counts)]) / sum(length_counts)
 print "Mean length of characters is", avg
 
-plt.hist(tweet_lengths)
+plt.hist(tweet_lengths, 50, facecolor='green', alpha=0.5, range=(1, 1000))
+plt.ylim(0, 1000)
 plt.title("Tweet Length Histogram")
 plt.xlabel("Tweet Length")
 plt.ylabel("Occurence")
-# plt.show()
+plt.show()
