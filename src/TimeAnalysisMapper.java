bed54d0705278e11cdeaf44f1bd39bf20b3962dc
Fayimora Femi-Balogun <fayi@fayimora.com>
2014-02-23 15:11:05 +0000

    Add initial Time Analysis code

---
+++ src/TimeAnalysisMapper.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.commons.lang.StringUtils;
+
+public class TimeAnalysisMapper extends Mapper<Object, Text, IntWritable, IntIntPair> {
+  /* private final IntWritable one = new IntWritable(1); */
+  /* private Text data = new Text(); */
+  /*  */
+  /* public void map(Object key, Text value, Context context) throws IOException, InterruptedException { */
+  /*   String dump = value.toString(); */
+  /*   if(StringUtils.ordinalIndexOf(dump, ";", 4) > -1) { */
+  /*     int startIndex = StringUtils.ordinalIndexOf(dump,";",3) + 1; */
+  /*     String tweet = dump.substring(startIndex, dump.lastIndexOf(';')); */
+  /*     IntIntPair pair = new IntIntPair(1, tweet.length()); */
+  /*     context.write(one, pair); */
+  /*   } */
+  /* } */
+}
---
+++ src/TimeAnalysisReducer.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class TimeAnalysisReducer extends Reducer<IntWritable, IntIntPair, IntWritable, IntWritable> {
+  /* private IntWritable result = new IntWritable(); */
+  /* public void reduce(IntWritable key, Iterable<IntIntPair> values, Context context) */
+  /*   throws IOException, InterruptedException { */
+  /*   int sum = 0; */
+  /*   int num = 0; */
+  /*   for (IntIntPair value : values) { */
+  /*     num += value.getFirst().get(); */
+  /*     sum += value.getSecond().get(); */
+  /*   } */
+  /*   int avg = sum/num; */
+  /*   result.set(avg); */
+  /*   context.write(key, result); */
+  /* } */
+}
